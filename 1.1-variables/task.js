b70cb66ba1f25821581ec11b9be9374301915f56
Vladislav Morozov <moroz0ff.vladislav@yandex.ru>
2021-04-03 11:19:31 +0400

    1.1v

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -7,21 +7,28 @@
 
 function averageMark(a, g, p) {
 	
-    // реализуйте здесь задание №1
+    let algebra = a;
+	let geography = g; 
+	let physics = p;
+	let averageRating = (algebra + geography + physics) / 3;
     
-	return // ваш ответ;
+	return averageRating
 }
 
 function sayHello(userName) {
 	
-    // реализуйте здесь задание №2
+    let myName = userName;
+	let message = 'Привет, мир! Меня зовут ' + myName;
     
-	return // ваш ответ;
+	return message
 }
 
 function calculateFormula() {
     
-	// реализуйте здесь задание №3
+	let x = 2,
+	    y = 22,
+		z = 0;
+	let result = x * y + 5 * z + x - 1;
     
-    return // ваш ответ;
+    return result
 }
