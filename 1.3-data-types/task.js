573b3a0b405151bf224caae39abd75557fe2cabb
Vladislav Morozov <moroz0ff.vladislav@yandex.ru>
2021-04-15 21:27:18 +0400

    v1.1

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,35 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    "use strict";
+    percent = Number(percent);
+    contribution = Number(contribution);
+    amount = Number(amount);
+    
+    let sumCredit = amount - contribution;
+    let P = percent / 12 / 100; //1/12 от процентной ставки
+
+    let indate = new Date;
+    let totalMonth = (date.getFullYear() - indate.getFullYear()) * 12 + (date.getMonth() - indate.getMonth()); // высщитывание кол-ва месяцев для оплаты ипотеки
+
+    let payMonth = sumCredit * (P + P / (((1 + P) ** totalMonth) - 1));
+    console.log(payMonth);
+
+    let totalAmount = payMonth * totalMonth;
+    console.log(totalAmount); 
+
+    
+    return Number(totalAmount.toFixed(2));
+    //return `Общая стоимость ипотеки: ${Number(totalAmount.toFixed(2))}, Первоначальный взнос: ${contribution}, Сумма основного долга: ${sumCredit}, Сумма за пользование кредитом: ${Number(totalAmount.toFixed(2)) - sumCredit}`;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    let greeting = '';
+
+    if(name > '') {
+        greeting = `Привет, мир! Меня зовут ${name}.`;
+    } else {
+        greeting = 'Привет, мир! Меня зовут Аноним.'
+    }
+    
+    console.log(greeting);
+    return greeting;
 }
